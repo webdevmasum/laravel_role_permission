dd78eaceb02e69351fc7622ae48af87cea9540aa
Md. Masum Miah <167614546+webdevmasum@users.noreply.github.com>
2025-02-21 20:04:12 +0600

    make tag controller

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function index()
+    {
+        return view('tags.index');
+    }
+}
